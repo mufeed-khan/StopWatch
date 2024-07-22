b8316e4a48db349e258333e54c693bef52d29953
Mufeed Khan <mufeedkhan192@gmail.com>
2024-07-22 11:40:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const display = document.getElementById('display');
+
+let timer = null;
+let startTimer = 0;
+let elapsedTimer = 0;
+let isRunning = false;
+
+function start() {
+  if (!isRunning) {
+    startTimer = Date.now() - elapsedTimer;
+    timer = setInterval(update, 10);
+    isRunning = true;
+  }
+}
+function stop() {
+  if (isRunning) {
+    clearInterval(timer);
+    elapsedTimer = Date.now() - startTimer;
+    isRunning = false;
+  }
+}
+function reset() {
+  clearInterval(timer);
+  startTimer = 0;
+  elapsedTimer = 0;
+  isRunning = false;
+  display.textContent = '00:00:00:00';
+}
+
+function update() {
+  const currentTime = Date.now();
+  elapsedTimer = currentTime - startTimer;
+
+  let hours = Math.floor(elapsedTimer / (1000 * 60 * 60));
+  let minutes = Math.floor(elapsedTimer / (1000 * 60) % 60);
+  let seconds = Math.floor(elapsedTimer / 1000 % 60);
+  let miliseconds = Math.floor(elapsedTimer % 1000 / 10);
+
+  hours = String(hours).padStart(2, '0');
+  minutes = String(minutes).padStart(2, '0');
+  seconds = String(seconds).padStart(2, '0');
+  miliseconds = String(miliseconds).padStart(2, '0');
+
+  display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+}
